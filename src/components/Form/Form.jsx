f78436c8c6859a4a417d85c974f33e1c67531044
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-05-30 15:22:09 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -36,6 +36,17 @@ export class Form extends Component {
             required
           />
         </label>
+        <label htmlFor="">
+          Number
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
       </form>
     );
   }
